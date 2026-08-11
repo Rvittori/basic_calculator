3942bac9f5c21fb823696ae1dd33972e34f284a3
Rita Vittori <ritavittori97@gmail.com>
2026-08-11 15:31:35 -0500

    Had to delete .idea as last resort as things were suddenly not working properly, all files showing as unversioned now. Commiting them again.

---
+++ out/artifacts/calculator_war_exploded/index.jsp
@@ -0,0 +1,88 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic Calculator</title>
+</head>
+<body>
+<form class="calculate-form" action="calculator" method="post">
+    <div class="clac-display-container">
+        <input id="display" name="display-value" type="text" readonly>
+
+    </div>
+    <div class="calculator-btn-container">
+        <div class="calculator-btn-row">
+            <div class="calculator-btn-col">
+                <button class="num-button" type="button" value="7" onclick="clickNum('7')">7</button>
+                <button class="num-button" type="button" value="8" onclick="clickNum('8')">8</button>
+                <button class="num-button" type="button" value="9" onclick="clickNum('9')">9</button>
+                <button class="num-button" type="button" value="*" onclick="clickOp('*')">x</button>
+            </div>
+        </div>
+        <div class="calculator-btn-row">
+            <div class="calculator-btn-col">
+                <button class="num-button" type="button" value="4" onclick="clickNum('4')">4</button>
+                <button class="num-button" type="button" value="5" onclick="clickNum('5')">5</button>
+                <button class="num-button" type="button" value="6" onclick="clickNum('6')">6</button>
+                <button class="num-button" type="button" value="-" onclick="clickOp('-')">-</button>
+            </div>
+        </div>
+        <div class="calculator-btn-row">
+            <div class="calculator-btn-col">
+                <button class="num-button" type="button" value="1" onclick="clickNum('1')">1</button>
+                <button class="num-button" type="button" value="2" onclick="clickNum('2')">2</button>
+                <button class="num-button" type="button" value="3" onclick="clickNum('3')">3</button>
+                <button class="num-button" type="button" value="+" onclick="clickOp('+')">+</button>
+            </div>
+        </div>
+        <div class="calculator-btn-row">
+            <div class="calculator-btn-col">
+                <button class="num-button" type="button" value="0" onclick="clickNum('0')">0</button>
+                <button class="num-button" type="button" value="." onclick="clickOp('.')">.</button>
+                <button class="num-button" type="button" value="/" onclick="clickOp('/')">/</button>
+                <button class="num-button" type="button" value="AC" onclick="clickOp('AC')">AC</button>
+                <input type="hidden" name="action" value="calculate">
+            </div>
+        </div>
+        <div class="calculator-btn-row">
+            <div class="calculator-btn-col">
+                <button class="num-button" type="submit" value="=">=</button>
+                <input type="hidden" name="action" value="calculate">
+            </div>
+            <div class="calculator-btn-row">
+                <div class="calculator-btn-col">
+                <c:if test="${message != null}">
+                    <p><i>${message}</i></p>
+                </c:if>
+            </div>
+            </div>
+
+        </div>
+    </div>
+</form>
+<script>
+    function clickNum(num) {
+        let display = document.getElementById("display");
+
+        display.value = display.value + num;
+    }
+
+    function clickOp(op) {
+        let display = document.getElementById("display");
+
+        if (op === "AC") {
+            display.value = "";
+        } else {
+            display.value = display.value + " " + op + " ";
+        }
+        document.getElementById("display").value = value + " " + op + " ";
+    }
+
+    function clickDel() {
+        let display = document.getElementById("display");
+        display.value = display.value.slice(0, -1)
+    }
+</script>
+</body>
+</html>
--- src/main/java/CalculatorServlet.java
+++ src/main/java/CalculatorServlet.java
@@ -3,8 +3,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Stack;
 
 
 @WebServlet("/calculator")
@@ -18,73 +19,59 @@ public class CalculatorServlet extends HttpServlet {
         if (action.equals("calculate")) {
             String expression = req.getParameter("display-value");
 
+            System.out.println("DEBUG: expression: " + expression);
             if (expression != null && !expression.isEmpty()) {
-                if (expression.contains("+")) {
-                    String[] expressionArray = expression.split("\\+");
 
-                    Calculator calculator = new Calculator();
-                    double num1 = Double.parseDouble(expressionArray[0]);
-                    double num2 = Double.parseDouble(expressionArray[1]);
-                    double result = calculator.addition(num1, num2);
-                    if (expressionArray.length > 2) {
-                        int i = 3;
-                        while (i < expressionArray.length) {
-                            num1 = i;
-                            result = calculator.addition(num1, result);
-                            i++;
-                        }
+                Stack<Double> output = new Stack<>();
+                Stack<String> operators = new Stack<>();
+
+                String[] expressionList = expression.split("");
+
+                for (int i = 0; i < expressionList.length; i++) {
+
+                    if (expressionList[i].equals(" ")) {
+                        continue;
+                    } else if (expressionList[i].equals("(") || expressionList[i].equals(")")){
+                        continue;
+                    } else if (isNumeric(expressionList[i])) {
+                        output.push(Double.parseDouble(expressionList[i]));
+
+                    } else if (expressionList[i].equals("+") || expressionList[i].equals("-") || expressionList[i].equals("*") || expressionList[i].equals("/") || expressionList[i].equals("^")) {
+                        operators.push(expressionList[i]);
                     }
-                } else if (expression.contains("-")) {
-                    String[] expressionArray = expression.split("-");
+                }
 
-                    Calculator calculator = new Calculator();
-                    double num1 = Double.parseDouble(expressionArray[0]);
-                    double num2 = Double.parseDouble(expressionArray[1]);
-                    double result = calculator.subtraction(num1, num2);
-                    if (expressionArray.length > 2) {
-                        int i = 3;
-                        while (i < expressionArray.length) {
-                            num1 = i;
-                            result = calculator.subtraction(num1, result);
-                            i++;
-                        }
-                    }
-                } else if (expression.contains("*")) {
-                        String[] expressionArray = expression.split("\\*");
+                for (int i: expressionArray) {
+                    System.out.println(i);
+                }
 
-                        Calculator calculator = new Calculator();
-                        double num1 = Double.parseDouble(expressionArray[0]);
-                        double num2 = Double.parseDouble(expressionArray[1]);
-                        double result = calculator.multiplication(num1, num2);
-                        if (expressionArray.length > 2) {
-                            int i = 3;
-                            while (i < expressionArray.length) {
-                                num1 = i;
-                                result = calculator.multiplication(num1, result);
-                                i++;
-                            }
-                        }
-                } else if (expression.contains("/")) {
-                    String[] expressionArray = expression.split("/");
-
-                    Calculator calculator = new Calculator();
-                    double num1 = Double.parseDouble(expressionArray[0]);
-                    double num2 = Double.parseDouble(expressionArray[1]);
-                    double result = calculator.multiplication(num1, num2);
-                    if (expressionArray.length > 2) {
-                        int i = 3;
-                        while (i < expressionArray.length) {
-                            num1 = i;
-                            result = calculator.division(num1, result);
-                            i++;
-                        }
-                    }
-                    req.setAttribute("result", result);
                     String url = "/index.jsp";
                     getServletContext().getRequestDispatcher(url)
                             .forward(req, resp);
                 }
             }
         }
+
+    private static boolean isNumeric(String s) {
+        if (s == null || s.isEmpty()) {
+            return false;
+        } else {
+            try {
+                Double.parseDouble(s);
+                return true;
+            } catch (NumberFormatException e){
+                return false;
+            }
+        }
+    }
+
+    private static int getPrecedence(char c) {
+        return switch (c) {
+            case '+', '-' -> 1;
+            case '*', '/' -> 2;
+            case '^' -> 3;
+            default -> -1;
+        };
     }
 }
+
