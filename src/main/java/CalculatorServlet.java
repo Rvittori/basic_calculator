0f833d61b35dd7ac29b21cc6a67c59b62b0bf9c6
Rita Vittori <ritavittori97@gmail.com>
2026-08-12 08:50:13 -0500

    Got the Shunting Yard alorigthm working! Do not go back beyond this point or it will break. Still working on getting calculations working.

--- src/main/java/CalculatorServlet.java
+++ src/main/java/CalculatorServlet.java
@@ -4,8 +4,8 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Stack;
+import java.lang.Math;
 
 
 @WebServlet("/calculator")
@@ -17,57 +17,41 @@ public class CalculatorServlet extends HttpServlet {
         String action = req.getParameter("action");
 
         if (action.equals("calculate")) {
-            String expression = req.getParameter("display-value");
 
-            System.out.println("DEBUG: expression: " + expression);
-            if (expression != null && !expression.isEmpty()) {
+            Stack<String> reversedPostfix = getReversedPostfix(req);
 
-                Stack<String> output = new Stack<>();
-                Stack<String> operators = new Stack<>();
-
-                String[] expressionList = expression.split("");
-
-
-                for (int i = 0; i < expressionList.length; i++) {
-
-                    if (expressionList[i].equals("(")) {
-                        operators.push(expressionList[i]);
-                    } else if (isNumeric(expressionList[i])) {
-                        output.push(expressionList[i]);
-                    } else if (expressionList[i].equals("+") || expressionList[i].equals("-") ||
-                            expressionList[i].equals("*") || expressionList[i].equals("/") ||
-                            expressionList[i].equals("^")) {
-
-                        if (operators.isEmpty()) {
-                            operators.push(expressionList[i]);
-                        } else {
-                            String currentExpressionItem = expressionList[i];
-                            int currentExpressionItemPrecedence = getPrecedence(currentExpressionItem);
-                            int lastItemOnOperatorsStackPrecedence =getPrecedence(operators.peek());
-
-                            if (currentExpressionItemPrecedence > lastItemOnOperatorsStackPrecedence) {
-                                output.push(currentExpressionItem);
-                            } else if (currentExpressionItemPrecedence <= lastItemOnOperatorsStackPrecedence) {
-                                String lastItemOnOperatorsStack = operators.pop();
-                                output.push(lastItemOnOperatorsStack);
-                                operators.push(currentExpressionItem);
-                            }
+            Calculator calculator = new Calculator();
+            int operand1;
+            int operand2 = 0;
+            String operator;
+            double result = 0.00;
+            while (!reversedPostfix.isEmpty()) {
+                if (isNumeric(reversedPostfix.peek())) {
+                    operand1  = Integer.parseInt(reversedPostfix.pop());
+                    if (isNumeric(reversedPostfix.peek())) {
+                        operand2 = Integer.parseInt(reversedPostfix.pop());
+                        while (isNumeric(reversedPostfix.peek())) {
+                            continue;
+                        }
+                        } if (!isNumeric(reversedPostfix.peek())) {
+                        operator = reversedPostfix.pop();
+                        switch (operator) {
+                            case "+" -> result = calculator.addition(operand1, operand2);
+                            case "-" -> result = calculator.subtraction(operand1, operand2);
+                            case "*" -> result = calculator.multiplication(operand1, operand2);
+                            case "/" -> result = calculator.division(operand1, operand2);
+                            case "^" -> result = Math.pow(operand1, operand2);
                         }
                     }
                 }
-                while (!operators.isEmpty()) {
-                    String currentOperatorItem = (operators.pop());
-                    output.push(currentOperatorItem);
-                }
-
-                System.out.print("Output stack: " + output);
-                System.out.print("Operators stack: " + operators);
-                    String url = "/index.jsp";
-                    getServletContext().getRequestDispatcher(url)
-                            .forward(req, resp);
-                }
             }
+            System.out.print(result);
+
+            String url = "/index.jsp";
+            getServletContext().getRequestDispatcher(url)
+                    .forward(req, resp);
         }
+    }
 
     private static boolean isNumeric(String s) {
         if (s == null || s.isEmpty()) {
@@ -76,7 +60,7 @@ public class CalculatorServlet extends HttpServlet {
             try {
                 Double.parseDouble(s);
                 return true;
-            } catch (NumberFormatException e){
+            } catch (NumberFormatException e) {
                 return false;
             }
         }
@@ -89,6 +73,79 @@ public class CalculatorServlet extends HttpServlet {
             case "^" -> 3;
             default -> -1;
         };
+    }
+
+
+    private Stack<String> getReversedPostfix(HttpServletRequest req) {
+        String expression = req.getParameter("display-value");
+        Stack<String> reversedPostfix = new Stack<>(); // so first off is start of expression
+
+        System.out.println("DEBUG: expression: " + expression);
+        if (expression != null && !expression.isEmpty()) {
+
+            Stack<String> output = new Stack<>();
+            Stack<String> operators = new Stack<>();
+
+            String[] expressionList = expression.split("");
+
+
+            int listLength = expressionList.length;
+            for (int i = 0; i < listLength; i++) {
+
+                if (expressionList[i].equals("(")) {
+                    operators.push(expressionList[i]);
+
+                } else if (isNumeric(expressionList[i])) {
+                    output.push(expressionList[i]);
+                } else if (expressionList[i].equals("+") || expressionList[i].equals("-") ||
+                        expressionList[i].equals("*") || expressionList[i].equals("/") ||
+                        expressionList[i].equals("^")) {
+
+                    if (operators.isEmpty()) {
+                        operators.push(expressionList[i]);
+                    } else {
+                        String currentExpressionItem = expressionList[i];
+                        int currentExpressionItemPrecedence = getPrecedence(currentExpressionItem);
+                        int lastItemOnOperatorsStackPrecedence = getPrecedence(operators.peek());
+
+                        if (currentExpressionItemPrecedence > lastItemOnOperatorsStackPrecedence) {
+                            operators.push(currentExpressionItem);
+                        } else if (currentExpressionItemPrecedence <= lastItemOnOperatorsStackPrecedence) {
+                            String lastItemOnOperatorsStack = operators.pop();
+                            output.push(lastItemOnOperatorsStack);
+                            operators.push(currentExpressionItem);
+                        }
+                    }
+                } else if (expressionList[i].equals(")")) {
+                    while (!operators.peek().equals("(")) {
+                        String currentOperatorStackItem = operators.pop();
+                        output.push(currentOperatorStackItem);
+                    }
+                    operators.pop();
+                    String currentOperatorStackItem = operators.pop();
+                    output.push(currentOperatorStackItem);
+                }
+            }
+            while (!operators.isEmpty()) {
+                String currentOperatorItem = (operators.pop());
+                if (currentOperatorItem.equals("(")) {
+                    continue;
+                }
+                output.push(currentOperatorItem);
+            }
+
+
+
+            while (!output.isEmpty()) {
+                reversedPostfix.push(output.pop());
+            }
+
+
+            System.out.print("Output stack: " + output);
+            System.out.print("Operators stack: " + operators);
+            System.out.print("ReverseStack: " + reversedPostfix);
+        }
+        return reversedPostfix;
     }
 }
 
